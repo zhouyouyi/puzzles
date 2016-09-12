81d6ad0a4f5f2b5522ad8186b7dd02b83f990549
Roy Zhou <zhou.youyi@gmail.com>
2016-09-12 17:13:28 +0800

    template code for codeforces

---
+++ codeforces/template.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <set>
+#include <map>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
