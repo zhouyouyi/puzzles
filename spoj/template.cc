4041ade135bdce0672d8f20a8707dd3d5832d43c
Roy Zhou <zhou.youyi@gmail.com>
2016-10-05 11:52:41 +0800

    update templates with c++11 headers

---
+++ spoj/template.cc
@@ -0,0 +1,21 @@
+// C++11
+
+#include <cstdint>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <set>
+#include <map>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
